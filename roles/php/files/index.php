4c1350ea1a7e838e87b01124f35bbbb2fd2dd110
root <root@ip-172-31-91-11.ec2.internal>
2018-08-21 23:45:00 +0530

    adding the playbook and the roles for apache and php7

---
+++ roles/php/files/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Welcome to Devops on AWS from PHP</title>
+</head>
+<body>
+	<h1><?php echo "welcome to devops on aws. This text wasgenerated using PHP 7" ?></h1>
+</body>
+</html>
